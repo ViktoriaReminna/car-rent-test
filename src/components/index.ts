207d0d1d0cfe1e394ebc40e01f47922cc284e75f
ViktoriaReminna <121427776+ViktoriaReminna@users.noreply.github.com>
2024-02-02 14:12:16 +0200

    Delete src directory

--- src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as App } from './App';
-export { Layout } from './Layout';
-export { Header } from './Header';
-
-export { Loader } from './Loader';
--- src/styles/index.ts
+++
@@ -1,2 +0,0 @@
-export { theme, palette } from './theme';
-export { default as GlobalStyles } from './GlobalStyles';
