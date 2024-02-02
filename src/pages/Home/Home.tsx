197b11a92cc0d16a09a998ce6e67f359a8d1537d
ViktoriaReminna <121427776+ViktoriaReminna@users.noreply.github.com>
2024-02-02 07:34:22 +0200

    Update Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,40 +1,14 @@
-import MainSlider from '../../components/MainSlider/MainSlider';
-import {
-  ContentWrapper,
-  SectionWrap,
-  SliderContainer,
-} from './MainPage.styled';
-import { Link } from 'react-router-dom';
-import { Gallary } from '../../components/Gallary/Gallery';
-import { useSelector } from 'react-redux';
-import { selectRecent } from '../../redux/likes/likesSelectors';
 
-export default function MainPage() {
-  const recentCars = useSelector(selectRecent);
-
-  const recentCarsSpliced = recentCars.slice(0, 4);
-
+const Home = () => {
   return (
-    <div>
-      <SliderContainer>
-        <MainSlider />
-      </SliderContainer>
-      <ContentWrapper>
-        <SectionWrap>
-          <p>Popular Cars</p>
-          <Link to="catalog">View All</Link>
-        </SectionWrap>
-        <CarsGallary />
-        {recentCars.length > 0 && (
-          <>
-            <SectionWrap>
-              <p>Recently Viewed Cars</p>
-              <Link to="catalog">View All</Link>
-            </SectionWrap>
-            <CarsGallary carsList={recentCarsSpliced} />
-          </>
-        )}
-      </ContentWrapper>
-    </div>
+    <>
+      
+      <div>
+        
+      
+      </div>
+    </>
   );
-}
+};
+
+export default Home;
